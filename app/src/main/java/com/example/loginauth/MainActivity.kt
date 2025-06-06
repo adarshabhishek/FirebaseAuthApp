7b33c2879b5282f7292fa0f499908844d42e191f
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-06-06 13:08:46 +0530

    Initial commit-Firebase Auth App

---
+++ app/src/main/java/com/example/loginauth/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.loginauth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.loginauth.ui.theme.LoginAuthTheme
+import com.google.firebase.FirebaseApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        enableEdgeToEdge()
+        setContent {
+            LoginAuthTheme {
+                AuthApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun AuthApp(){
+    val navController= rememberNavController()
+    NavHost(navController, startDestination = "login") {
+        composable("login") {LoginScreen(navController) }
+        composable("signup") { SignupScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+    }
+}
