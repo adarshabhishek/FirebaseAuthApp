a29385136e9bc0b31a44ac9e92d95c1d34798303
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-06-07 15:57:03 +0530

    Forgot Password functionality added.

--- app/src/main/java/com/example/loginauth/LoginScreen.kt
+++ app/src/main/java/com/example/loginauth/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.loginauth
 
 
+import android.app.AlertDialog
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -32,6 +34,7 @@ import com.google.firebase.auth.auth
 fun LoginScreen(navController: NavHostController){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var forgotPassword by remember { mutableStateOf(false) }
 
     Box(modifier = Modifier.fillMaxSize().padding(16.dp),
         contentAlignment = Alignment.Center){
@@ -78,7 +81,61 @@ fun LoginScreen(navController: NavHostController){
                 Text("Login")
             }
             Spacer(modifier = Modifier.height(8.dp))
+            if (forgotPassword) {
+                var resetEmail by remember { mutableStateOf("") }
+                val context = LocalContext.current
 
+                AlertDialog(
+                    title = { Text("Forgot Password") },
+                    text = {
+                        OutlinedTextField(
+                            value = resetEmail,
+                            onValueChange = { resetEmail = it },
+                            label = { Text("Email") },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    },
+                    confirmButton = {
+                        TextButton(onClick = {
+                            if (resetEmail.isNotEmpty()) {
+                                Firebase.auth.sendPasswordResetEmail(resetEmail)
+                                    .addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            Toast.makeText(
+                                                context,
+                                                "Password reset email sent",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                            forgotPassword = false
+                                        } else {
+                                            Toast.makeText(
+                                                context,
+                                                "Registered email not found.",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    "Please enter your registered email",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }) { Text("Submit") }
+                    }, dismissButton = {
+                        TextButton(onClick = { forgotPassword = false }) {
+                            Text("Cancel")
+                        }
+                    }, onDismissRequest = { forgotPassword = false }
+                )
+            }
+
+            TextButton(onClick = {forgotPassword=true})  {
+                Text("Forgot Password?")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
             TextButton(onClick = {
                 navController.navigate("signup") {
                     popUpTo("login") { inclusive = true }
